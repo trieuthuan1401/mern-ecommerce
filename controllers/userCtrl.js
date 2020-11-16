ba061bbade17fb56c598a97f55abd4b952ed201e
lllieutrieuthuan <lieutrieuthuan14@gmail.com>
2020-11-17 01:20:28 +0700

    api category success

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,25 +1,28 @@
 //Controllers là nơi mình chứa các function được thực hiện khi xử lí các request và trả về
 
-
-const Users = require('../models/userModel')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
+const Users = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const userCtrl = {
   register: async (req, res) => {
     try {
       const { name, email, password } = req.body
       const user = await Users.findOne({ email })
-      if (user) return res.status(400).json({ msg: "The email already exists." })
+      if (user)
+        return res.status(400).json({ msg: "The email already exists." })
       if (password.length < 6) {
-        return res.status(400).json({ msg: "Password is at least 6 characters long." })
+        return res
+          .status(400)
+          .json({ msg: "Password is at least 6 characters long." })
       }
 
       //Password Encryption
       const passwordhHash = await bcrypt.hash(password, 10)
       const newUser = new Users({
-        name, email, password: passwordhHash
+        name,
+        email,
+        password: passwordhHash,
       })
       //Save mongoDB
       await newUser.save()
@@ -28,24 +31,23 @@ const userCtrl = {
       const accesstoken = createAccessToken({ id: newUser._id })
       const refreshtoken = createRefreshToken({ id: newUser._id })
 
-      res.cookie('refreshtoken', refreshtoken, {
+      res.cookie("refreshtoken", refreshtoken, {
         httpOnly: true,
-        path: '/user/refresh_token'
+        path: "/user/refresh_token",
       })
 
       res.json({ accesstoken })
-
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
   },
 
-
   login: async (req, res) => {
     try {
-      const { email, password } = req.body;
+      const { email, password } = req.body
 
       const user = await Users.findOne({ email })
+      // console.log('123')
       if (!user) return res.status(400).json({ msg: "User does not exist." })
 
       const isMatch = await bcrypt.compare(password, user.password)
@@ -55,14 +57,13 @@ const userCtrl = {
       const accesstoken = createAccessToken({ id: user._id })
       const refreshtoken = createRefreshToken({ id: user._id })
 
-      res.cookie('refreshtoken', refreshtoken, {
+      res.cookie("refreshtoken", refreshtoken, {
         httpOnly: true,
-        path: '/user/refresh_token',
+        path: "/user/refresh_token",
         // maxAge: 7 * 24 * 60 * 60 * 1000 // 7d
       })
 
       res.json({ accesstoken })
-
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
@@ -70,8 +71,8 @@ const userCtrl = {
 
   logout: async (req, res) => {
     try {
-      res.clearCookie('refreshtoken', { path: '/user/refresh_token' })
-      return res.json({ msg: 'Logged out' })
+      res.clearCookie("refreshtoken", { path: "/user/refresh_token" })
+      return res.json({ msg: "Logged out" })
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
@@ -79,43 +80,40 @@ const userCtrl = {
 
   refreshToken: (req, res) => {
     try {
-      const rf_token = req.cookies.refreshtoken;
-      if (!rf_token) return res.status(400).json({ msg: "Please Login or Register" })
+      const rf_token = req.cookies.refreshtoken
+      if (!rf_token)
+        return res.status(400).json({ msg: "Please Login or Register" })
 
       jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(400).json({ msg: "Please Login or Register" })
+        if (err)
+          return res.status(400).json({ msg: "Please Login or Register" })
 
         const accesstoken = createAccessToken({ id: user.id })
 
         res.json({ accesstoken })
       })
-
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
-
   },
 
   getUser: async (req, res) => {
     try {
-      const user = await Users.findById(req.user.id).select('-password')
+      const user = await Users.findById(req.user.id).select("-password")
       if (!user) return res.status(400).json({ msg: "User doesn't exist." })
       res.json(user)
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
-  }
-
-
-
+  },
 }
 
 const createAccessToken = (user) => {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
 }
 
 const createRefreshToken = (user) => {
-  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" })
 }
 
 module.exports = userCtrl
--- server.js
+++ server.js
@@ -1,44 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
-const path = require('path')
-
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const fileUpload = require("express-fileupload")
+const cookieParser = require("cookie-parser")
+const path = require("path")
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
-app.use(fileUpload({
-  useTempFiles: true
-}))
-
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+)
 
 //Routes
-app.use('/user', require('./routes/userRouter'))
-
-
-
+app.use("/user", require("./routes/userRouter"))
+app.use("/api", require("./routes/categoryRouter"))
+// app.use("*", (req, res) => {
+//   return res.send("not found")
+// })
 
 //Connect to MongoDB
 const URI = process.env.MONGODB_URL
-mongoose.connect(URI, {
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, err => {
-  if (err) throw err;
-  console.log('Connected to MongoDB')
-})
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err
+    console.log("Connected to MongoDB")
+  }
+)
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({ msg: "lllieutrieuthuan" })
 })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
+  console.log("Server is running on port", PORT)
 })
